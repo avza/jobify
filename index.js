508dd252f83acc7fe3588a7ac6d426d53169f111
Alisson Souza <alissonvsouza8@gmail.com>
2019-11-27 19:03:55 -0200

    Adding port

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open('banco.sqlite', { Promise })
+
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -124,7 +127,7 @@ const init = async () => {
 
 init()
 
-app.listen(3001, (err) => {
+app.listen(port, (err) => {
     if (err) {
         console.log('Não foi possível iniciar o servidor do jobify')
     } else {
